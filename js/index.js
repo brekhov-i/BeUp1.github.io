44cc2a6293bd68c60d4b01b7bff01fa7b0dec27e
Илья Брехов <ilabrehov@MacBook-Air-Ila.local>
2020-07-02 17:31:35 +0300

    сделал стрелочку

--- js/index.js
+++ js/index.js
@@ -1,15 +1,49 @@
 'use strict'
 
 const type = document.querySelector('.type');
+const arrow = document.querySelector('.arrow');
+const arrowFill = arrow.querySelector('.fill');
 
 var typed = new Typed(type, {
     stringsElement: '#typed-strings',
-    typeSpeed: 20,
-    backSpeed: 20,
+    typeSpeed: 10,
+    backSpeed: 10,
     startDelay: 300,
-    backDelay: 1000,
+    backDelay: 2000,
     loop: true,
     showCursor: false,
     smartBackspace: false,
     fadeOut: false,
+    onComplete: function (self) {
+        if (arrow.classList.contains('reset')) {
+            arrow.classList.remove('reset')
+        }
+        self.pause.status = true;
+        arrow.classList.add('animated')
+        setTimeout(() => {
+            self.pause.status = false;
+            arrow.classList.remove('animated')
+            arrow.classList.add('reset')
+        }, 2000)
+    },
 });
+function animate({ timing, draw, duration }) {
+
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        // timeFraction изменяется от 0 до 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // вычисление текущего состояния анимации
+        let progress = timing(timeFraction);
+
+        draw(progress); // отрисовать её
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+    });
+}
