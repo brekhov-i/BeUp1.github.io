157f6ab7df53dd607cb067cdaa45095593b978b4
Илья Брехов <ilabrehov@MacBook-Air-Ila.local>
2020-07-03 11:25:51 +0300

    parallax

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
 'use strict'
 
 const type = document.querySelector('.type');
-const arrow = document.querySelector('.arrow');
-const arrowFill = arrow.querySelector('.fill');
 
 var typed = new Typed(type, {
     stringsElement: '#typed-strings',
-    typeSpeed: 10,
-    backSpeed: 10,
+    typeSpeed: 100,
+    backSpeed: 100,
     startDelay: 300,
     backDelay: 2000,
     loop: true,
     showCursor: false,
-    smartBackspace: false,
+    smartBackspace: true,
     fadeOut: false,
+    onComplete: (self) => {
+        //self.pause.status = true;
+    }
+});
+
+
+var wow = new WOW(
+    {
+        boxClass: 'wow',      // animated element css class (default is wow)
+        animateClass: 'animate__animated', // animation css class (default is animated)
+        offset: 100,          // distance to the element when triggering the animation (default is 0)
+        mobile: true,       // trigger animations on mobile devices (default is true)
+        live: true,
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
+    }
+);
+wow.init();
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
 });
