fcf54cd5b389dae97a97530169f949c4921251f3
Илья Брехов <ilabrehov@MacBook-Air-Ila.local>
2020-07-03 10:16:47 +0300

    jfdbf

--- js/index.js
+++ js/index.js
@@ -14,36 +14,4 @@ var typed = new Typed(type, {
     showCursor: false,
     smartBackspace: false,
     fadeOut: false,
-    onComplete: function (self) {
-        if (arrow.classList.contains('reset')) {
-            arrow.classList.remove('reset')
-        }
-        self.pause.status = true;
-        arrow.classList.add('animated')
-        setTimeout(() => {
-            self.pause.status = false;
-            arrow.classList.remove('animated')
-            arrow.classList.add('reset')
-        }, 2000)
-    },
 });
-function animate({ timing, draw, duration }) {
-
-    let start = performance.now();
-
-    requestAnimationFrame(function animate(time) {
-        // timeFraction изменяется от 0 до 1
-        let timeFraction = (time - start) / duration;
-        if (timeFraction > 1) timeFraction = 1;
-
-        // вычисление текущего состояния анимации
-        let progress = timing(timeFraction);
-
-        draw(progress); // отрисовать её
-
-        if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-        }
-
-    });
-}
